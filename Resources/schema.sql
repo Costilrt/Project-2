ff5b5ad445d4409e9a3836769e46ce9bd165d74c
wawilson810 <wawilson810@gmail.com>
2022-06-24 15:27:22 -0400

    Finished schema and loading the data

---
+++ Resources/schema.sql
@@ -0,0 +1,22 @@
+create table vaccinations (
+	id int primary key,
+	iso_code varchar(10),
+	continent varchar(20),
+	location varchar(50),
+	date date,
+	population float(15),
+	total_vaccinations float(15),
+	total_boosters float(15),
+	perc_vaccinated float(15),
+	perc_fully_vaccinated float(15)
+);
+
+create table stats (
+	"Country" varchar(30) primary key,
+	"GDP" float(10),
+	"Population Density" float(5),
+	"Urbanization" float(5),
+	"Unemployment" float(5)
+);
+
+select * from stats;
